b6f46c73a7866456b730cd73191d2fbfaa4c4729
huifang <592546574@qq.com>
2018-12-13 14:34:03 +0800

    练习

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2 class="title">{{msg}}</h2>
+</template>
+
+<script>
+    export default {
+        name: "HelloWorld",
+      data(){
+         return{
+           msg:'Kobe Tom'
+         }
+      }
+    }
+</script>
+<style>
+  .title{
+    color: greenyellow;
+  }
+</style>
